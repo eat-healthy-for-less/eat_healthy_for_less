1bb7deee2697c2b5f4c452f6a44654557bcf82c5
Trey Smith <trey.smith@nasa.gov>
2013-06-01 18:52:59 -0700

    fixtures

--- ehfl/recipe.py
+++ ehfl/recipe.py
@@ -7,12 +7,46 @@ from glob import glob
 PARENT_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '')
 RECIPES_DIR = PARENT_DIR + 'recipes/'
 
+def convertToDotDictRecurse(struct):
+    if isinstance(struct, dict):
+        for k, v in struct.iteritems():
+            struct[k] = convertToDotDictRecurse(v)
+        return DotDict(struct)
+    elif isinstance(struct, list):
+        return [convertToDotDictRecurse(elt) for elt in struct]
+    else:
+        return struct
+
+
+class DotDict(dict):
+    # At the moment this object exists pretty much solely to let you
+    # get and set elements in its __dict__ dictionary via dotted
+    # notation.  Someday it could do more.
+
+    # these are fields that must not be defined to avoid causing problems
+    # with Django
+    _badFields = ('prepare_database_save',)
+
+    def copy(self):
+        return DotDict(self)
+
+    def __repr__(self):
+        return json.dumps(self, sort_keys=True, indent=4)
+
+    def __getattr__(self, attr):
+        if attr.startswith('__'):
+            raise AttributeError(attr)  # avoids breaking copy.deepcopy
+        if attr in self._badFields:
+            raise KeyError(attr)
+        return self.get(attr, None)
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
 def read_recipe(path):
-    return json.loads(open(path, 'r').read())
+    return convertToDotDictRecurse(json.loads(open(path, 'r').read()))
 
 
 def read_recipes():
     paths = glob(RECIPES_DIR + '*.json')
     return [read_recipe(p) for p in paths]
-
-
